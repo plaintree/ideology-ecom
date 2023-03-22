d664f827ab8809d22c36851c359eb1e858b7d4a3
plaintree <cheukhei04@hotmail.com>
2023-03-22 22:32:13 +0000

    finish basic styling of landing page

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="bg-gray-100 min-h-28 px-8 py-4">
+    <section class="flex justify-evenly sm:gap-4">
+      <div class="flex flex-col">
+        <h3 class="text-gray-700 text-lg mb-8">About</h3>
+        <ul class="list-none leading-7 text-sm">
+          <li>.ideology</li>
+          <li>stores</li>
+          <li>content</li>
+        </ul>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="text-gray-700 text-lg mb-8">Help</h3>
+        <ul class="list-none leading-7 text-sm">
+          <li>Shipping & Returns</li>
+          <li>FAQ</li>
+          <li>Customer Service</li>
+        </ul>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="text-gray-700 text-lg mb-8">Social</h3>
+        <ul class="list-none leading-7 text-sm">
+          <li>Instagram</li>
+          <li>LinkedIn</li>
+          <li>Spotify</li>
+        </ul>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="text-gray-700 text-lg mb-8">Legal</h3>
+        <ul class="list-none leading-7 text-sm">
+          <li>Terms & Conditions</li>
+          <li>Private Policy</li>
+          <li>Legal mentions</li>
+        </ul>
+      </div>
+    </section>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="flex shadow-lg items-center px-4 sm:px-8 py-4">
+    <img src="/logo.png" alt="logo" class="w-20 sm:w-40 md:w-52 mx-8" />
+    <div class="hidden sm:flex sm:gap-8">
+      <a
+        href="/collections/new-arrival"
+        class="border-b border-b-white hover:border-b-slate-900 hover:transition-all"
+        >New Arrival</a
+      >
+      <a
+        href="/collections/women"
+        class="border-b border-b-white hover:border-b-slate-900 hover:transition-all"
+        >Women</a
+      >
+      <a
+        href="/collections/men"
+        class="border-b border-b-white hover:border-b-slate-900 hover:transition-all"
+        >Men</a
+      >
+      <a
+        href="/about"
+        class="border-b border-b-white hover:border-b-slate-900 hover:transition-all"
+        >About</a
+      >
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
