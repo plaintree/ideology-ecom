d45f21c11daa66d671ee7c1ce67e26ac94330293
plaintree <cheukhei04@hotmail.com>
2023-03-27 21:44:43 +0100

    change to SSR mode

---
+++ src/components/CardItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="link-card" href="#">
+    <img
+      :src="image"
+      :alt="title"
+      class="w-[150px] h-[200px] sm:h-[250px] sm:w-[200px] mb-4"
+    />
+    <h5 class="uppercase tracking-wider">{{ title }}</h5>
+  </a>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  title?: string;
+  image: string;
+}
+
+const { title, image } = defineProps(["title", "image"]);
+</script>
