3400516f5fe8158ab9d400c0fbb72a62bc4c5d18
plaintree <cheukhei04@hotmail.com>
2023-03-28 15:20:09 +0100

    update styling on prductdetail and about page

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -1,18 +1,18 @@
 <template>
   <section
-    class="flex flex-col sm:flex-row justify-center sm:justify-between m-4 sm:my-6"
+    class="flex flex-col sm:flex-row justify-center sm:justify-between m-4 sm:m-16"
   >
     <img
       :src="product.image"
       :alt="product.title"
       class="mb-4 sm:w-[500px] sm:mx-10 my-5 sm:my-0"
     />
-    <div class="flex flex-col">
+    <div class="flex flex-col sm:px-8">
       <h5 class="text-lg mb-2">{{ product.title }}</h5>
       <h5 class="mb-2">${{ product.price }}</h5>
       <p class="text-sm tracking-normal leading-5">{{ product.description }}</p>
       <button
-        class="px-6 py-2 my-4 rounded-none bg-black hover:bg-white border border-black text-white hover:text-black transition duration-300"
+        class="px-6 sm:px-4 py-2 my-4 max-w-sm rounded-none bg-black hover:bg-white border border-black text-white hover:text-black transition duration-300"
       >
         Add to Bag
       </button>
