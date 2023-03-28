6ae4526ac347da092b8769c3f36834b01041d68b
plaintree <cheukhei04@hotmail.com>
2023-03-28 15:04:04 +0100

    change deploy server config to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
-import netlify from "@astrojs/netlify/edge-functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [vue(), tailwind()],
-  adapter: netlify(),
+  adapter: vercel(),
 });
