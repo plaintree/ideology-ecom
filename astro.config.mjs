cc07598223a51703acd07341d1146f3b3bee504e
plaintree <cheukhei04@hotmail.com>
2023-03-28 14:17:53 +0100

    change netlify to edge-function

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [vue(), tailwind()],
-  adapter: netlify()
+  adapter: netlify(),
 });
